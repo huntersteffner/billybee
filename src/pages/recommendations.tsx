8a688859fd79952ed9dba4833418a528c33c42a5
huntersteffner <huntersteffner@gmail.com>
2023-03-22 12:00:26 -0400

    Started outline

---
+++ src/pages/recommendations.tsx
@@ -0,0 +1,11 @@
+const Recommendations = () => {
+    return (
+        <>
+            <div className="container mx-auto">
+                <h1 className="">Cap'n Billy's Favorites (besides fishing)</h1>
+            </div>
+        </>
+    )
+}
+
+export default Recommendations
