3a07547ede5f74d5171ba1b46ef5060c7f3b00ae
huntersteffner <huntersteffner@gmail.com>
2023-03-23 10:01:14 -0400

    Created pages

--- src/pages/recommendations.tsx
+++ src/pages/recommendations.tsx
@@ -1,11 +1,16 @@
 const Recommendations = () => {
-    return (
-        <>
-            <div className="container mx-auto">
-                <h1 className="">Cap'n Billy's Favorites (besides fishing)</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="">Cap'n Billy's Favorites (besides fishing)</h1>
+        <div>
+          Favorite "laid back" food{' '}
+          <a href="https://www.gnatslanding.com/">Gnats Landing</a>
+          <p>310 Redfern Village</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Recommendations
